23f57e402f7c70110cce3798605c2c95a608fe29
Vladislav Komanchy <vkomanchy@gmail.com>
2023-10-10 10:07:57 +0300

    feat: change icon, change manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -120,11 +120,9 @@ module.exports = {
         short_name: `Tech blog`,
         start_url: `/`,
         background_color: `#ffffff`,
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
+        theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/site-icon.png`, // This path is relative to the root of the site.
       },
     },
   ],
