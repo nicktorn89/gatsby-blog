e46318dbe9e14ab73b13714154d8f8fd85690c2c
Vladislav Komanchy <vkomanchy@gmail.com>
2024-04-01 23:21:43 +0300

    feat: add description and other info to widget

--- src/components/ExperienceWidget/ExperienceWidget.jsx
+++ src/components/ExperienceWidget/ExperienceWidget.jsx
@@ -4,24 +4,52 @@ import { DateTime } from 'luxon';
 import experienceData from './experienceData.json';
 
 import './style.css';
+import { WorkDescription } from './WorkDescription/WorkDescription';
 
 export const ExperienceWidget = () => {
   return (
     <div className='experience-widget-container'>
       {experienceData.map(
-        ({ id, startDate, endDate, workplace, workDescription }) => {
-          const startDateFormatted = new DateTime(startDate).toLocaleString();
-          const endDateFormatted = new DateTime(endDate).toLocaleString();
-          
+        ({
+          id,
+          startDate,
+          endDate,
+          workplace,
+          workDescription,
+          workplaceLink,
+          positionName,
+        }) => {
+          const startDateFormatted = new DateTime(startDate).toLocaleString(DateTime.DATE_FULL);
+          const endDateFormatted = new DateTime(
+            endDate || new Date(),
+          ).toLocaleString(DateTime.DATE_FULL);
+
           return (
             <div key={id} id={id} className='workplace-item'>
               <div className='workplace-dates'>
                 <p className='workplace-dates-text'>
-                  {startDateFormatted} - 
-                  {endDateFormatted}
+                  {startDateFormatted} -
+                  {endDate ? endDateFormatted : 'Currently'}
                 </p>
 
                 <div className='workplaces-line' />
+              </div>
+
+              <div className='workplace-description'>
+                <h4 className='workplace-title'>{workplace}</h4>
+                {workplaceLink && (
+                  <a
+                    href={workplaceLink}
+                    target='_blank'
+                    rel='noopener noreferrer'
+                  >
+                    {workplaceLink}
+                  </a>
+                )}
+
+                <h4 className='workplace-position-name'>{positionName}</h4>
+
+                <WorkDescription descriptions={workDescription} />
               </div>
             </div>
           );
