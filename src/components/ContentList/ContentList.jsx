5b64480bf3a8c8e059c5caecd30d7967d48e72e0
Vladislav Komanchy <vkomanchy@gmail.com>
2024-04-09 20:42:02 +0300

    feat: add getting all titles logic

--- src/components/ContentList/ContentList.jsx
+++ src/components/ContentList/ContentList.jsx
@@ -1,17 +1,29 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 export const ContentList = ({ postHtml }) => {
+  const [allTitles, setAllTitles] = useState(null);
+
   const postElementRef = useRef();
 
-  console.log('postHtml', postHtml);
+  console.log('allTitles', allTitles);
 
   useEffect(() => {
     if (postElementRef.current) {
       console.log('found element', postElementRef.current);
 
-      const allTitles = postElementRef.current.querySelectorAll('h2');
+      const titlesTags = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'];
 
-      console.log('allTitles', allTitles)
+      const allTitles = titlesTags.reduce((acc, titleTag) => {
+        if (acc[titleTag]) {
+          acc[titleTag].push(...postElementRef.current.querySelectorAll(titleTag));
+        } else {
+          acc[titleTag] = Array.from(postElementRef.current.querySelectorAll(titleTag));
+        }
+
+        return acc;
+      }, {});
+
+      setAllTitles(allTitles);
     }
   }, [postElementRef.current]);
 
@@ -22,6 +34,8 @@ export const ContentList = ({ postHtml }) => {
         dangerouslySetInnerHTML={{ __html: postHtml }}
         ref={postElementRef}
       ></div>
+
+      {allTitles && <div>All titles</div>}
     </div>
   );
 };
